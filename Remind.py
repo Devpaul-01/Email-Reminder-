1aadcb727c5c243a576b837ae0c41cddab1d1c32
Ogunsola Toluwase Paul <oluwaseyiogunsola90@gmail.com>
2025-09-15 00:51:32 +0100

    TestRemind.py

--- Remind.py
+++ Remind.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def send_email(subject, body, to_email):
+    #made changes
     email_address = os.getenv('EMAIL_ADDRESS')
     email_password = os.getenv('EMAIL_PASSWORD')
     smtp_server = os.getenv('SMTP_SERVER', 'smtp.gmail.com')
